717b8bd571b20aa9673c768bafabf49d72124194
Mostafa <mostafas@stud.ntnu.no>
2025-09-22 12:40:33 +0200

    add viewing of first rows

--- 1/a.py
+++ 1/a.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import numpy as np
 
+energy_df = pd.read_csv("datasets/energy_dataset.csv")
+weather_df = pd.read_csv("datasets/weather_features.csv")
+
+print(energy_df.head())
+print(weather_df.head())
 
